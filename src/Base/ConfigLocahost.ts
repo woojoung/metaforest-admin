8ac77f3f16140a7f645a30c4bd49f6a0901f5ff6
Woojoung <chae3229@gmail.com>
2022-04-06 16:53:48 +0900

    Create ConfigLocahost.ts

---
+++ src/Base/ConfigLocahost.ts
@@ -0,0 +1,18 @@
+
+import { ConfigBase } from './ConfigBase'
+import { eEnvironment } from '../Enums/Environment'
+
+export class ConfigLocalhost extends ConfigBase {
+    constructor() {
+        super()
+
+        this.environment = eEnvironment.LOCALHOST
+        this.publicPath = '/public/'
+
+        this.locale = 'ko-KR'
+
+        this.apiUrl = 'http://127.0.0.1:7071'
+    }
+}
+
+export const cfg = new ConfigLocalhost()
