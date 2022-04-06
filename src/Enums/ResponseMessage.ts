6475543c185e0c5f1766e6b423da2056b29a395f
Woojoung <chae3229@gmail.com>
2022-04-06 16:54:11 +0900

    Create ResponseMessage.ts

---
+++ src/Enums/ResponseMessage.ts
@@ -0,0 +1,19 @@
+
+export enum eResponseMessage {
+    OK = '',
+    CREATED = 'Created',
+
+    FOUND = 'Found',
+
+    BAD_REQUEST = 'Bad Request',
+    UNAUTHORIZED = 'Unauthorized',
+    PAYMENT_REQUIRED = 'Payment Required',
+    FORBIDDEN = 'Forbidden',
+    NOT_FOUND = 'Not Found',
+    METHOD_NOT_ALLOWED = 'Method Not Allowed',
+    CONFLICT = 'Conflict',
+    UPGRADE_REQUIRED = 'Upgrade Required',
+
+    INTERNAL_SERVER_ERROR = 'Internal Server Error',
+    NETWORK_AUTHENTICATION_REQUIRED = 'Network Authentication Required',
+}
