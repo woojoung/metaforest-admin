a672dee4dcb691b594d86fca33d5884eb5232d9a
Woojoung <chae3229@gmail.com>
2022-04-06 16:55:34 +0900

    Create ApiResponse.ts

---
+++ src/Libs/ApiRequest.ts
@@ -0,0 +1,60 @@
+
+import { Any, integer } from '../Base/Type'
+import { eApiMessageType } from '../Enums/ApiMessageType'
+
+export class ApiRequest {
+    msgType: eApiMessageType
+    data: Any
+
+    constructor( msgType = 0, data: Any = null) {
+        this.msgType = msgType
+        this.data = data
+    }
+
+    getBoolean(name: string, defaultValue = false): boolean {
+        if (this.data === null || (name in this.data) === false) {
+            return defaultValue
+        }
+
+        const value = this.data[name]
+        if (value === null) {
+            return defaultValue
+        }
+
+        return typeof value === 'boolean' ? value : value.toString() === 'true'
+    }
+
+    getInteger(name: string, defaultValue = 0): number {
+        if (this.data === null || (name in this.data) === false) {
+            return defaultValue
+        }
+
+        let value = this.data[name]
+        if (value === null) {
+            return defaultValue
+        }
+
+        if (typeof value !== 'string') {
+            value = value.toString()
+        }
+
+        if (value === '') {
+            return defaultValue
+        }
+
+        return integer(value)
+    }
+
+    getString(name: string, defaultValue = ''): string {
+        if (this.data === null || (name in this.data) === false) {
+            return defaultValue
+        }
+
+        const value = this.data[name]
+        if (value === null || value === '') {
+            return defaultValue
+        }
+
+        return typeof value === 'string' ? value : value.toString()
+    }
+}
