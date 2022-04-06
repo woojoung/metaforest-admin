9d3561b0499dbc7e3cc23339bafa635d25c224f2
Woojoung <chae3229@gmail.com>
2022-04-06 16:54:01 +0900

    Create AccessLevel.ts

---
+++ src/Enums/AccessLevel.ts
@@ -0,0 +1,10 @@
+
+export enum eAccessLevel {
+    UNKNOWN = 0,
+    USER = 10,
+    COUNSELOR = 20,
+    SERVICE_OPERATOR = 30,
+    SERVICE_ADMIN = 40,
+    SYSTEM_OPERATOR = 50,
+    SYSTEM_ADMIN = 60,
+}
