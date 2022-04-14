9af2cd890b81c472be6bd2e8954224b5e8007f73
Woojoung <chae3229@gmail.com>
2022-04-14 16:40:45 +0900

    Update sh_deploy_production_build.sh

--- sh_deploy_production_build.sh
+++ sh_deploy_production_build.sh
@@ -31,7 +31,7 @@ cp -rf ./dist ../admin_mf_production_$DATE
 
 cp -f ./src/Base/ConfigLocalhost.ts ./src/Base/Config.ts
 
-aws s3 sync ../admin_mf_production_$DATE s3://adminmetaforest --delete
+aws s3 sync ../admin_mf_production_$DATE s3://adminmetaforest --delete --profile mfdeployer
 
 git tag admin_mf_production_$DATE\_$NAME
 git push origin admin_mf_production_$DATE\_$NAME
