38c5d91e1cb20240ed28bdcecd63c31da5487622
Woojoung <chae3229@gmail.com>
2022-05-04 20:25:43 +0900

    Update cfg.production.js

--- public/tools/cfg.production.js
+++ public/tools/cfg.production.js
@@ -3,5 +3,6 @@
 
 const env = 'production'
 
-const apiUrl = 'http://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
+// const apiUrl = 'http://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
+const apiUrl = 'https://ec2-api.metaforest.us:7071/'
 
