7c89695e817132bba97ed1e681ca7c0d32706527
Woojoung <chae3229@gmail.com>
2022-06-09 16:28:08 +0900

    imported date picker css

--- src/Elements/Admin/PartnerForm.tsx
+++ src/Elements/Admin/PartnerForm.tsx
@@ -16,6 +16,7 @@ import { nowStr, toLocalTimeStr, newTime } from '../../Base/Time'
 import { Partners } from '../../Models/Partners'
 import { ePlanType } from '../../Enums/PlanType'
 import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 import { ko } from 'date-fns/esm/locale'
 
 export const AdminPartnerForm: FC = (): JSX.Element => {
