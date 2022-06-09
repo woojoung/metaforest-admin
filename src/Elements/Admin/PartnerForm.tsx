b520e5daf72baf022851a1987a98c0648223ad00
Woojoung <chae3229@gmail.com>
2022-06-09 17:03:45 +0900

    Update PartnerForm.tsx

--- src/Elements/Admin/PartnerForm.tsx
+++ src/Elements/Admin/PartnerForm.tsx
@@ -16,7 +16,8 @@ import { nowStr, toLocalTimeStr, newTime } from '../../Base/Time'
 import { Partners } from '../../Models/Partners'
 import { ePlanType } from '../../Enums/PlanType'
 import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+// import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker-cssmodules.min.css'
 import { ko } from 'date-fns/esm/locale'
 
 export const AdminPartnerForm: FC = (): JSX.Element => {
